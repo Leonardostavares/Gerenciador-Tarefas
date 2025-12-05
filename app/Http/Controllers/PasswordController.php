d7a236776f7957b34b76205c6d4d066e390d308f
Leonardostavares <leosilvatavares13@gmail.com>
2025-12-05 09:58:13 -0300

    Eliminando espacos

--- app/Http/Controllers/PasswordController.php
+++ app/Http/Controllers/PasswordController.php
@@ -17,7 +17,6 @@ class PasswordController extends Controller
         
         $request->validate([
             'email' => 'required|email',
-        
         ]);
         
         $user = DB::select ('SELECT * FROM users WHERE email = ?', [$request->email]);
